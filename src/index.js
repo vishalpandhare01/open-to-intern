7c3f9b2372637c450c1a4a0719d8f302344107b7
palsubodh <palsubodh390@gmail.com>
2022-11-21 12:20:23 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { mongoose } = require('mongoose')
+const route = require('./routes/route')
 const app = express()
 
 app.use(express.json())
@@ -7,3 +8,9 @@ app.use(express.json())
 mongoose.connect("mongodb+srv://palsubodh:Palsubodh@cluster0.mhegah9.mongodb.net/Intern",{useNewUrlParser:true})
 .then(()=>console.log("mongoDb is connected"))
 .catch((err)=>console.log(err))
+
+app.listen(3000,function(){
+    console.log("Port is running on 3000 ")
+})
+console.log("hi")
+app.use("/",route)
